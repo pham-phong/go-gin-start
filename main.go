064d8117c75ed49d73968731a9598e42fe47ec46
Phạm Phong <phongpv@rabiloo.com.vn>
2021-02-03 17:20:44 +0700

    change routes

--- main.go
+++ main.go
@@ -10,7 +10,13 @@ func main() {
 	db := database.ConnectDB()
 	db.AutoMigrate(&models.User{})
 
-	api.CreateApiUrl(db)
-	// Listen and server on 0.0.0.0:8080
-	api.Router.Run(":8080")
+	// r := routes.CreateApiUrl(db)
+	// // Listen and server on 0.0.0.0:8080
+	// r.Run(":8080")
+
+	r := routes.SetupRoute(db)
+
+	api := routes.CreateApiUrl(r)
+
+	api.Run(":8080")
 }
--- routes/api.go
+++ routes/api.go
@@ -1,35 +1,53 @@
-package api
+package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
+	// "github.com/jinzhu/gorm"
 	"modules/controllers"
 	"modules/middleware"
+	// cors "github.com/rs/cors/wrapper/gin"
 )
 
-var Router *gin.Engine
+// var Router *gin.Engine
 
-func CreateApiUrl(db *gorm.DB) {
-	Router = gin.Default()
+// func CreateApiUrl(db *gorm.DB) *gin.Engine {
+// 	Router := gin.New()
 
-	// Provide db variable to controllers
-	Router.Use(func(c *gin.Context) {
-		c.Set("db", db)
-		c.Next()
-	})
-	// Router of the API
-	r := Router.Group("/api")
+// 	Router.Use(cors.AllowAll())
+// 	// Provide db variable to controllers
+// 	Router.Use(func(c *gin.Context) {
+// 		c.Set("db", db)
+// 		c.Next()
+// 	})
+// 	// Router of the API
+// 	r := Router.Group("/api")
+// 	{
+// 		r.POST("/login/", controllers.Login)
+
+// 		r.Use(middleware.AuthorizeJWT())
+// 		{
+// 			r.GET("/users", controllers.GetUser)
+// 			r.POST("/user", controllers.SaveUser)
+// 			r.GET("/user/:id", controllers.FindUser)
+// 			r.PUT("/user/:id", controllers.UpdateUser)
+// 			r.DELETE("/logout", controllers.Logout)
+// 		}
+
+// 	}
+// 	return Router
+// }
+
+func CreateApiUrl(r *gin.Engine) *gin.Engine {
+	r.POST("/api/login", controllers.Login)
+
+	auth := r.Group("/api")
+	auth.Use(middleware.AuthorizeJWT())
 	{
-		r.POST("/login/", controllers.Login)
-
-		r.Use(middleware.AuthorizeJWT())
-		{
-			r.GET("/user", controllers.GetUser)
-			r.POST("/user", controllers.SaveUser)
-			r.GET("/user/:id", controllers.FindUser)
-			r.PUT("/user/:id", controllers.UpdateUser)
-			r.DELETE("/logout", controllers.Logout)
-		}
-
+		r.GET("/users", controllers.GetUser)
+		r.POST("/user", controllers.SaveUser)
+		r.GET("/user/:id", controllers.FindUser)
+		r.PUT("/user/:id", controllers.UpdateUser)
+		r.DELETE("/logout", controllers.Logout)
 	}
+	return r
 }
